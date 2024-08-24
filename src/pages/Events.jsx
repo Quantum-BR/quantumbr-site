779aa313dcb3ce8ce271fe2afcb8bc5899c9ce3e
Justin Woodring <jwoodrg@gmail.com>
2024-08-23 20:50:43 -0500

    Update Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -5,9 +5,9 @@ export default function Events(){
         <div className="content">
             <h1>Events</h1>
             <h2>Opening Night</h2>
-            <h6>When: Wed Aug 28th, 2023 from 5:30pm to 7:30</h6>
-            <h6>Where: Patrick F. Taylor Hall @ LSU, Room 1263</h6>
-            <img src={openingnight}/>
+            <h3></h3>When: Wed Aug 28th, 2023 from 5:30pm to 7:30</h3>
+            <h3>Where: Patrick F. Taylor Hall @ LSU, Room 1263</h3>
+            <img style="margin:auto" src={openingnight}/>
             <p>
                 QuantumBR’s opening night, with icebreakers, some lightning presentations and demos.
                 If you’re interested in the local quantum computing community be sure not to miss this event!
