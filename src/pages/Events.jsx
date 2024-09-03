8f562f13f0a51ff59b864584952fa5d1f1919ca9
Justin Woodring <jwoodrg@gmail.com>
2024-09-02 19:12:34 -0500

    Update Events.jsx

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,13 +1,22 @@
 import openingnight from "../assets/bitmap.png"
+import biweeklymeeting from "../assets/bitmap2.png"
 
 export default function Events(){
     return(
         <div className="content">
             <h1>Events</h1>
-            <h2>Opening Night</h2>
-            <h3>When: Wed Aug 28th, 2023 from 5:30pm to 7:30</h3>
+            <h2>Biweekly Meeting</h2>
+            <h3>When: Wed Sep 18th, 2024 from 5:30pm to 7:30</h3>
             <h3>Where: Patrick F. Taylor Hall @ LSU, Room 1263</h3>
-            <img style={{margin:"auto"}} src={openingnight}/>
+            <img src={biweeklymeeting} width={600}/>
+            <p>
+                The biweekly QuantumBR meeting featuring speakers, workshops,
+                icebreakers, and other quantum-focused activities.
+            </p>
+            <h1>Past Events</h1>
+            <h2>Opening Night</h2>
+            <h3>When: Wed Aug 28th, 2024 from 5:30pm to 7:30</h3>
+            <h3>Where: Patrick F. Taylor Hall @ LSU, Room 1263</h3>
             <p>
                 QuantumBR’s opening night, with icebreakers, some lightning presentations and demos.
                 If you’re interested in the local quantum computing community be sure not to miss this event!
