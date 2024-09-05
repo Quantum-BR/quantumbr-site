e2c4d33474a90584e32cc8229362aa485167f227
Justin Woodring <jwoodrg@gmail.com>
2024-09-04 22:18:49 -0500

    Update for Qiskit Fall Fest

--- src/pages/FallFest.jsx
+++ src/pages/FallFest.jsx
@@ -39,37 +39,37 @@ export default function FallFest(){
             <h1>Quantum Week Starts October 7th!</h1>
             <ul>
             <li>
-            Wednesday - October 2nd - Get Ready For Fall Fest!!<br/>
-            (Meetup) (5:30-7:30) <br/>
+            <h3>Wednesday - October 2nd - Get Ready For Fall Fest!!</h3><br/>
+            (Meetup) (5:30pm-7:30pm CDT) <br/>
             Location TBA
             </li> 
             <br/>
             <li>
-            Monday - October 7th – Quantum Week Kickoff<br/>
+            <h3>Monday - October 7th – Quantum Week Kickoff</h3><br/>
             (Virtual) (Notebook Drop) (Hackathon Announcement) (10:00am CDT)<br/>
             <a href="https://discord.gg/Nxr2NQkyWp?event=1281080307549999115">Discord Virtual Event Link</a>
             </li>
             <br/>
             <li>
-            Wednesday - October 9th– IBM Quantum Speaker / Workshopping <br/>
-            (Meetup) (Virtual Speaker) (5:30pm-7:30pm) <br/>
+            <h3>Wednesday - October 9th– Quantum Speaker / Workshopping</h3><br/>
+            (Meetup) (5:30pm-7:30pm CDT) <br/>
             Location TBA 
             </li>
             <br/>
             <li>
-            Friday - October 11th – Notebooks due  / Hackathon Projects Start <br/>
-            (Virtual) (11:59PM) 
+            <h3>Friday - October 11th – Notebooks due  / Hackathon Projects Start</h3><br/>
+            (Deadline) (Hackathon Drop) (11:59pm CDT) 
             </li>
             <br/>
             <li>
-            Tuesday - October 15th – Hackathon Projects Due <br/>
-            (Virtual) (11:59PM) 
+            <h3>Tuesday - October 15th – Hackathon Projects Due</h3><br/>
+            (Deadline) (11:59pm CDT) 
             </li>
             <br/>
             <li>
             Wednesday - October 16th – QuantumBR Qiskit Fall Fest Closing Ceremony <br/>
-            (Hybrid) (5:30-7:30) <br/>
-            <a href="https://discord.gg/rET2xdWm?event=1281080307549999115">Discord Virtual Event Link</a>
+            (Hybrid) (5:30pm-7:30pm CDT) <br/>
+            <a href="https://discord.gg/rET2xdWm?event=1281080307549999115">Discord Virtual Event Link</a><br/>
             Location TBA
             </li>
             </ul>
