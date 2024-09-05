6716f68c131849220f755c61e520f2a07256e31b
Justin Woodring <jwoodrg@gmail.com>
2024-09-04 22:22:27 -0500

    Update for Qiskit Fall Fest

--- src/pages/FallFest.jsx
+++ src/pages/FallFest.jsx
@@ -39,35 +39,35 @@ export default function FallFest(){
             <h1>Quantum Week Starts October 7th!</h1>
             <ul>
             <li>
-            <h3>Wednesday - October 2nd - Get Ready For Fall Fest!!</h3><br/>
+            <h3>Wednesday - October 2nd - Get Ready For Fall Fest!!</h3>
             (Meetup) (5:30pm-7:30pm CDT) <br/>
             Location TBA
             </li> 
             <br/>
             <li>
-            <h3>Monday - October 7th – Quantum Week Kickoff</h3><br/>
+            <h3>Monday - October 7th – Quantum Week Kickoff</h3>
             (Virtual) (Notebook Drop) (Hackathon Announcement) (10:00am CDT)<br/>
             <a href="https://discord.gg/Nxr2NQkyWp?event=1281080307549999115">Discord Virtual Event Link</a>
             </li>
             <br/>
             <li>
-            <h3>Wednesday - October 9th– Quantum Speaker / Workshopping</h3><br/>
+            <h3>Wednesday - October 9th– Quantum Speaker / Workshopping</h3>
             (Meetup) (5:30pm-7:30pm CDT) <br/>
             Location TBA 
             </li>
             <br/>
             <li>
-            <h3>Friday - October 11th – Notebooks due  / Hackathon Projects Start</h3><br/>
+            <h3>Friday - October 11th – Notebooks due  / Hackathon Projects Start</h3>
             (Deadline) (Hackathon Drop) (11:59pm CDT) 
             </li>
             <br/>
             <li>
-            <h3>Tuesday - October 15th – Hackathon Projects Due</h3><br/>
+            <h3>Tuesday - October 15th – Hackathon Projects Due</h3>
             (Deadline) (11:59pm CDT) 
             </li>
             <br/>
             <li>
-            <h3>Wednesday - October 16th – QuantumBR Qiskit Fall Fest Closing Ceremony</h3><br/>
+            <h3>Wednesday - October 16th – QuantumBR Qiskit Fall Fest Closing Ceremony</h3>
             (Hybrid) (5:30pm-7:30pm CDT) <br/>
             <a href="https://discord.gg/rET2xdWm?event=1281080307549999115">Discord Virtual Event Link</a><br/>
             Location TBA
