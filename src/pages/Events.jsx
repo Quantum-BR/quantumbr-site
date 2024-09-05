e1ac83d22149e1eeee042eedf04629f186691660
Justin Woodring <jwoodrg@gmail.com>
2024-09-05 08:33:02 -0500

    Update for Qiskit Fall Fest

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -6,14 +6,14 @@ export default function Events(){
     return(
         <div className="content">
             <h1>Events</h1>
-            <h1>QuantumBR x IBM Extension Qiskit Fall Fest '24</h1>
+            <h2>QuantumBR x IBM Extension Qiskit Fall Fest '24</h2>
             <p>Details: <Link to="/ff24">Fall Fest Event Schedule</Link></p>
-
+            <hr/>
             <h2>Get Ready For Fall Fest!!</h2>
             <h3>When: Wed Oct 2nd from 5:30pm-7:30pm CDT</h3>
             <h3>Where: Patrick F. Taylor Hall @ LSU, Room 1200</h3>
             <p>Interested in Quantum Week? Have questions? Be sure to stop by and get ready for Quantum Week's challenges!</p>
-            
+            <hr/>
             <h2>Biweekly Meeting</h2>
             <h3>When: Wed Sep 18th, 2024 from 5:30pm to 7:30</h3>
             <h3>Where: Patrick F. Taylor Hall @ LSU, Room 1263</h3>
